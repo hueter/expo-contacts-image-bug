d80fa42892849957105e11afc00183a6205a9f7b
Michael Hueter <michael@numero.ai>
2023-10-30 20:44:17 -0400

    :bug:

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as Contacts from "expo-contacts";
 
 export default function App() {
+  const handleSaveContact = useCallback(async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status !== "granted") {
+      return;
+    }
+
+    await Contacts.presentFormAsync(null, {
+      [Contacts.Fields.ID]: "1",
+      [Contacts.Fields.ContactType]: Contacts.ContactTypes.Person,
+      [Contacts.Fields.Name]: "Test Contact",
+      [Contacts.Fields.Image]: {
+        uri: "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403",
+      },
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity
+        onPress={handleSaveContact}
+        style={{ padding: 10, backgroundColor: "#4a44fa" }}
+      >
+        <Text>Save this contact</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
