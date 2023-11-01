9449b27f91c89b1c7d6dc7eb801334cb7869ec5c
Michael Hueter <michael@numero.ai>
2023-10-31 20:06:24 -0400

    fixed with FileSystem.downloadAsync

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import * as Contacts from "expo-contacts";
+import * as FileSystem from "expo-file-system";
 
 export default function App() {
   const handleSaveContact = useCallback(async () => {
@@ -10,12 +11,17 @@ export default function App() {
       return;
     }
 
+    const imageUri = await FileSystem.downloadAsync(
+      "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403",
+      FileSystem.cacheDirectory + "/images"
+    );
+
     await Contacts.presentFormAsync(null, {
       [Contacts.Fields.ID]: "1",
       [Contacts.Fields.ContactType]: Contacts.ContactTypes.Person,
       [Contacts.Fields.Name]: "Test Contact",
       [Contacts.Fields.Image]: {
-        uri: "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403",
+        uri: imageUri.uri,
       },
     });
   }, []);
